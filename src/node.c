ptomato/inform7-ide

commit e680138427025747009a3e38861922e594d24ded

Use new generic positioning interface in GooCanvas
git-svn-id: https://gnome-inform7.svn.sourceforge.net/svnroot/gnome-inform7/trunk@419 7a7d6849-5d27-4570-9ef9-9f38b82aaeff

src/node.c
@@ -701,9 +701,8 @@ i7_node_layout(I7Node *self, GooCanvasItemModel *skein, GooCanvas *canvas, gdoub
 
 	/* Move the node's group to its proper place */
 	gdouble y = (gdouble)(g_node_depth(self->gnode) - 1.0) * vspacing;
-	/* SUCKY DEBIAN set_simple_transform -> x,y properties */
-	goo_canvas_item_model_set_simple_transform(GOO_CANVAS_ITEM_MODEL(self), x, y, 1.0, 0.0);
-
+	g_object_set(self, "x", x, "y", y, NULL);
+	
 	/* Cache the x coordinate */
 	priv->x = x;
 }
@@ -728,11 +727,14 @@ i7_node_calculate_size(I7Node *self, GooCanvasItemModel *skein, GooCanvas *canva
 
 	if((width != 0.0 && priv->command_width != width) || (height != 0.0 && priv->command_height != height)) {
 		/* Move the label, its background, and the differs badge */
-		/* SUCKY DEBIAN we have to do this with set_simple_transform, because x and
-		y properties don't yet exist */
-		goo_canvas_item_model_set_simple_transform(priv->label_item, 0.0, -height, 1.0, 0.0);
-		goo_canvas_item_model_set_simple_transform(priv->label_shape_item, 0.0, -height, 1.0, 0.0);
-		goo_canvas_item_model_set_simple_transform(priv->badge_item, width / 2 + DIFFERS_BADGE_WIDTH, height / 2, DIFFERS_BADGE_WIDTH, 0.0);
+		g_object_set(priv->label_item, "x", 0.0, "y", -height, NULL);
+		g_object_set(priv->label_shape_item, "x", 0.0, "y", -height, NULL);
+		g_object_set(priv->badge_item, 
+			"x", width / 2 + DIFFERS_BADGE_WIDTH,
+			"y", height / 2,
+			"width", DIFFERS_BADGE_WIDTH,
+			"height", DIFFERS_BADGE_WIDTH,
+			NULL);
 
 		/* Calculate the scale for the pattern gradients */
 		cairo_matrix_init_scale(&matrix, 0.5 / width, 1.0 / height);

commit c49b0f87d4d64b805370594c9baa604c675f59d0

Fixed differs badge size and position
Regression introduced in [e6801384]

src/node.c
@@ -25,7 +25,7 @@
 #include "node.h"
 #include "skein.h"
 
-#define DIFFERS_BADGE_WIDTH 8.0
+#define DIFFERS_BADGE_RADIUS 8.0
 
 enum {
 	PROP_0,
@@ -862,10 +862,10 @@ i7_node_calculate_size(I7Node *self, GooCanvasItemModel *skein, GooCanvas *canva
 		
 		if(command_width_changed || command_height_changed)
 			g_object_set(priv->badge_item, 
-				"x", command_width / 2 + DIFFERS_BADGE_WIDTH,
-				"y", command_height / 2,
-				"width", DIFFERS_BADGE_WIDTH,
-				"height", DIFFERS_BADGE_WIDTH,
+				"x", command_width / 2,
+				"y", command_height / 2 - DIFFERS_BADGE_RADIUS,
+				"width", DIFFERS_BADGE_RADIUS * 2,
+				"height", DIFFERS_BADGE_RADIUS * 2,
 				NULL);
 	} else
 		g_object_set(priv->badge_item, "visibility", GOO_CANVAS_ITEM_HIDDEN, NULL);