82aa2c915db8e4566a60c2eee7e0e9a18b328f0d
Philip Chimento <philip.chimento@gmail.com>
2022-03-13 21:30:53 -0700

    Replace copyright headers with SPDX comments
    This is more concise, less clutter, and machine-readable.

--- src/app.c
+++ src/app.c
@@ -1,18 +1,6 @@
-/*  Copyright (C) 2007-2015, 2019, 2021 P. F. Chimento
- *  This file is part of GNOME Inform 7.
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-FileCopyrightText: 2007-2015, 2019, 2021 Philip Chimento <philip.chimento@gmail.com>
  */
 
 #include "config.h"
@@ -209,7 +197,7 @@ i7_app_init(I7App *self)
 	priv->print_settings = NULL;
 	priv->page_setup = gtk_page_setup_new();
 
-	/* Create the Gnome Inform7 dir if it doesn't already exist */
+	/* Create the Inform dir if it doesn't already exist */
 	GFile *extensions_file = i7_app_get_extension_file(self, NULL, NULL);
 	if(!make_directory_unless_exists(extensions_file, NULL, &error)) {
 		IO_ERROR_DIALOG(NULL, extensions_file, error, _("creating the Inform directory"));
--- src/story-index.c
+++ src/story-index.c
@@ -1,18 +1,6 @@
-/* Copyright (C) 2006-2009, 2010, 2011, 2012, 2014 P. F. Chimento
- * This file is part of GNOME Inform 7.
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+/*
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * SPDX-FileCopyrightText: 2006-2012, 2014 Philip Chimento <philip.chimento@gmail.com>
  */
 
 #include "config.h"
