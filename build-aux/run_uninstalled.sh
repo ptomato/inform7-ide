a57f206cc53596dbaaf5cec3add1478dbbb5fab7
Philip Chimento <philip.chimento@gmail.com>
2022-09-04 10:37:28 -0700

    Fix icons when running uninstalled

--- build-aux/run_uninstalled.sh
+++ build-aux/run_uninstalled.sh
@@ -6,4 +6,5 @@ INFORM7_IDE_DATA_DIR=_install/usr/local/share/inform7-ide \
     GSETTINGS_SCHEMA_DIR=_build \
     INFORM7_IDE_LIBEXEC_DIR=_install/usr/local/libexec/inform7-ide \
     LD_LIBRARY_PATH=_build/subprojects/chimara/libchimara:_build/subprojects/ratify \
+    XDG_DATA_DIRS=$XDG_DATA_DIRS:_install/usr/local/share \
     $DEBUGGER _build/src/inform7-ide
